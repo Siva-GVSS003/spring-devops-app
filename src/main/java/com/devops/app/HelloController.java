9dff63b4f6d2c303defd4e719260e43bd2174505
Siva-GVSS003 <sivagvs1998@gmail.com>
2026-04-19 23:02:30 +0530

    added ansible vault

--- src/main/java/com/devops/app/HelloController.java
+++ src/main/java/com/devops/app/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
     // run this method and return the result
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Boot - DevOps Pipeline v4-new!";
+        return "Hello from Spring Boot - DevOps Pipeline v4!";
     }
 
     // A health check endpoint - useful to verify app is running
