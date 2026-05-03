652bf79d69a705fabdc8679fea48aef4401b91f2
Siva-GVSS003 <sivagvs1998@gmail.com>
2026-05-03 19:58:59 +0530

    dev  branch commit 1

--- src/test/java/com/devops/app/HelloControllerTest.java
+++ src/test/java/com/devops/app/HelloControllerTest.java
@@ -20,7 +20,7 @@ public class HelloControllerTest {
         mockMvc.perform(get("/hello"))
             .andExpect(status().isOk())
             .andExpect(content().string(
-                "Hello from Spring Boot - DevOps dev Pipeline v1.1!"));
+                "Hello from Spring Boot - DevOps dev Pipeline v1.2!"));
     }
 
     @Test
