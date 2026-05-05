a61578bad798618102862a4c72ae6e1d6bad4b27
Siva-GVSS003 <sivagvs1998@gmail.com>
2026-05-05 18:12:40 +0530

    Pipeline investigation

--- src/test/java/com/devops/app/HelloControllerTest.java
+++ src/test/java/com/devops/app/HelloControllerTest.java
@@ -20,7 +20,7 @@ public class HelloControllerTest {
         mockMvc.perform(get("/hello"))
             .andExpect(status().isOk())
             .andExpect(content().string(
-                "Hello from Spring Boot - DevOps dev Pipeline v1!"));
+                "Hello from Spring Boot - DevOps dev Pipeline v2!"));
     }
 
     @Test
